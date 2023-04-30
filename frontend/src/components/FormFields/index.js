0cf340f74983d3f44f7f95c8842a6f354f388f43
Leandro Palmeira <leandropalmeira@hotmail.com>
2023-04-30 01:28:55 -0300

    Delete frontend directory

--- frontend/src/components/FormFields/index.js
+++
@@ -1,5 +0,0 @@
-import InputField from './InputField';
-import CheckboxField from './CheckboxField';
-import SelectField from './SelectField';
-import DatePickerField from './DatePickerField';
-export { InputField, CheckboxField, SelectField, DatePickerField };
