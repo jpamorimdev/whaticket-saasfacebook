f208d84fabee363741c0d0491e3ed0c6617303b9
Leandro Palmeira <leandropalmeira@hotmail.com>
2023-05-14 22:25:27 -0300

    Add files via upload

--- backend/src/helpers/GetTicketWbot.ts
+++ backend/src/helpers/GetTicketWbot.ts
@@ -1,23 +1,22 @@
-import { AnyWASocket } from "@adiwajshing/baileys";
+import { WASocket } from "@adiwajshing/baileys";
 import { getWbot } from "../libs/wbot";
 import GetDefaultWhatsApp from "./GetDefaultWhatsApp";
 import Ticket from "../models/Ticket";
 import { Store } from "../libs/store";
 
-type Session = AnyWASocket & {
+type Session = WASocket & {
   id?: number;
   store?: Store;
 };
 
 const GetTicketWbot = async (ticket: Ticket): Promise<Session> => {
   if (!ticket.whatsappId) {
-    const defaultWhatsapp = await GetDefaultWhatsApp(ticket.companyId);
+    const defaultWhatsapp = await GetDefaultWhatsApp(ticket.user.id);
 
     await ticket.$set("whatsapp", defaultWhatsapp);
   }
 
   const wbot = getWbot(ticket.whatsappId);
-
   return wbot;
 };
 
